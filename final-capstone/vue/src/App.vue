5b99a028d20e610bd95c463c54db30e2e0cb2eaa
Tiffany McGillvary <tmcgillvary94@gmail.com>
2021-12-04 10:40:09 -0500

    made it so guests can see the homepage, got the login box on the home page

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -2,8 +2,23 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
+      <login />
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import Login from "@/views/Login.vue";
+
+export default {
+  components: {
+    Login,
+  },
+};
+</script>
--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <!-- <p>You must be authenticated to see this</p> -->
+    <p>now you can do it without logging in(?)</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
