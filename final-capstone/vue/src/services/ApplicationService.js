82957e5bf8373f35de11f441a1ab7fe8e0414936
Angela Livezey <angelalivezey96@gmail.com>
2021-12-09 10:27:09 -0500

    Added the Logged In view components. they are now all visible on the home page

--- final-capstone/vue/src/services/ApplicationService.js
+++ final-capstone/vue/src/services/ApplicationService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
 
     // add new Volunteer Form to Database
-    // TODO: insert link from server side
+
     submitForm(application) {
         return axios.post("/volunteerapplication", application)
     }
