b4de5576540f737316ec709c7467b523d29eca91
Tiffany McGillvary <tmcgillvary94@gmail.com>
2021-12-13 13:31:44 -0500

    little styling things, got the logged in menu to hide when not logged in

--- final-capstone/vue/src/views/LoggedInView.vue
+++ final-capstone/vue/src/views/LoggedInView.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="logged-in-form">
-    <router-link v-bind:to="{ name: 'add-pet' }" class="links"
+  <div id="logged-in-form" v-if="$store.state.token != ''">
+    <router-link
+      tag="button"
+      v-bind:to="{ name: 'add-pet' }"
+      id="add"
+      class="button-style"
       >Add Pet</router-link
     >
-    <router-link v-bind:to="{ name: 'volunteer-directory' }" class="links"
-      >Volunteer Directory</router-link
+    <router-link
+      tag="button"
+      v-bind:to="{ name: 'volunteer-directory' }"
+      id="directory"
+      class="button-style"
+      >Directory</router-link
     >
   </div>
 </template>
@@ -14,4 +22,20 @@ export default {};
 </script>
 
 <style>
+#add {
+  grid-area: add;
+}
+
+#directory {
+  grid-area: directory;
+}
+
+#logged-in-form {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  margin-top: 15px;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: "add directory";
+}
 </style>
